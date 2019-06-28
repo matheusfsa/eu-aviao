bd42e914072b0b913e5dae10f4f5730a435aca47
Thome <thomeneto@hotmail.com>
2019-06-28 00:56:18 -0300

    luz fixa no sol

--- cenario.cpp
+++ cenario.cpp
@@ -4,39 +4,36 @@
 #include <string>
 using namespace std;
 float posCameraX,posCameraY,posCameraZ, raio, angulo;
-
-void init (void) 
+void init(void) 
 {
-    cout << "Please enter the radius of the planet: ";
-    cin >> raio; //ex: 0.5
-    cout << "Please enter the camera's starting X Y Z: ";
-    cin >> posCameraX >> posCameraY >> posCameraZ; //ex: 2 1 1
+   cout << "Please enter the radius of the planet: ";
+   cin >> raio; //ex: 0.5
+   cout << "Please enter the camera's starting X Y Z: ";
+   cin >> posCameraX >> posCameraY >> posCameraZ; //ex: 2 1 1
+
+   GLfloat light_position[] = { 1.0, 0.0, 0.0, 1.0 };
+   GLfloat light_diffuse[] = {1.0, 1.0, 0.0, 1.0};    
+   GLfloat lmodel_ambient[] = { 0.3, 0.3, 0.3, 1.0 };
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glShadeModel (GL_SMOOTH);
+
+   /*  initialize viewing values  */
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(-2.0, 2.0, -2.0, 2.0, -1, 100.0);
    
-    /*  initialize viewing values  */
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-2.0, 2.0, -2.0, 2.0, -1, 100.0);
-    //gluPerspective(120.0, 1, 0.5, 1000.0);
-    glMatrixMode (GL_MODELVIEW);
-    glLoadIdentity();
-     
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    glShadeModel (GL_SMOOTH);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+   glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+   glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
 
-    GLfloat light_position[] = {1.0, 0.0, 0.0, 1.0};
-    GLfloat light_diffuse[] = {1.0, 1.0, 0.0, 1.0};
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_DEPTH_TEST);
 
-    GLfloat lmodel_ambient[] = { 0.3, 0.3, 0.3, 1.0 };
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
-    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
-        
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_DEPTH_TEST);
 }
+
 void specialKeys(int key, int x, int y)
 {
    float angulo = 2*M_PI/180;
@@ -57,52 +54,76 @@ void specialKeys(int key, int x, int y)
    }
    glutPostRedisplay();
 }
+
 void display(void)
 {
    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
- 
+   GLfloat light_position[] = { 0, 1, -1.0, 1.0 };
+
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity();
+   
    gluLookAt (posCameraX, posCameraY, posCameraZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-   glColor3f (0.2, 0.3, 1.0);
-   glutSolidSphere(raio,20,20);
+   glLightfv(GL_LIGHT0, GL_POSITION, light_position);   
+   
+   glColor3f (0.7, 0.7, 1.0);
+   glutSolidSphere (raio, 20, 16);   
+
    glPushMatrix();
    	glTranslatef(0.0,  1.0, -1.0);   	 
         glColor3f (1.0, 1.0, 0.0);
    	glutSolidSphere(0.2,20,20);
-        //GLfloat light_position[] = {0.0, 0.0, 0.0, 1.0};
-	//GLfloat light_diffuse[] = {1.0, 1.0, 0.0, 1.0};
-	//glLightfv(GL_LIGHT1, GL_POSITION, light_position);
-	//glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse);
    glPopMatrix();
-   
-   glBegin(GL_LINES);
-      glColor3f(1.0, 0.0, 0.0);
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(1.0, 0.0, 0.0);
 
-      glColor3f(0.0, 1.0, 0.0);
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(0.0, 1.0, 0.0);
+//glutWireTeapot(0.5);
+   //glutWireSphere(0.5,20,20);
+   //visualização dos eixos
+   //exercício: faça um plano sob a chaleira
+   glBegin(GL_LINES);
+        glColor3f (1.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(1.0, 0.0, 0.0);
+        
+        glColor3f (0.0, 1.0, 0.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 1.0, 0.0);
       
-      glColor3f(0.0, 0.0, 1.0);
-      glVertex3f(0.0, 0.0, 0.0);
-      glVertex3f(0.0, 0.0, 1.0);
-   glEnd();
+        glColor3f (0.0, 0.0, 1.0);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, 1.0);    
+    glEnd();
+    
    //troca de buffers, o flush é implícito aqui
-   glutSwapBuffers();
+   glutSwapBuffers();   
+
 }
 
-int main(int argc, char** argv){
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize (500, 500); 
-    glutInitWindowPosition (500, 100);
-    glutCreateWindow (argv[0]);
+void reshape (int w, int h)
+{
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity();
+   if (w <= h)
+      glOrtho (-1.5, 1.5, -1.5*(GLfloat)h/(GLfloat)w,
+         1.5*(GLfloat)h/(GLfloat)w, -10.0, 10.0);
+   else
+      glOrtho (-1.5*(GLfloat)w/(GLfloat)h,
+         1.5*(GLfloat)w/(GLfloat)h, -1.5, 1.5, -10.0, 10.0);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+}
 
-    init();
-    glutDisplayFunc(display); 
-    glutSpecialFunc(specialKeys);
-    glutMainLoop();
-    
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+   glutInitWindowSize (500, 500); 
+   glutInitWindowPosition (500, 100);
+   glutCreateWindow (argv[0]);
+   init ();
+   glutDisplayFunc(display); 
+   glutSpecialFunc(specialKeys);
+   glutReshapeFunc(reshape);
+   glutMainLoop();
+   return 0;
 }
